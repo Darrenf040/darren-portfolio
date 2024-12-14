22941bc304095d7534877c84464d6475d42e9b6b
Darrenf040 <darrenfr83@gmail.com>
2024-12-13 19:41:16 -0600

    gloabl styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#1B1B1B", //background color
+        "secondary-color": "#EEF8CF",
+        "accent-color": "#D1FF48",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      devil: ["Devil Breeze", "Anniversary", "sans-serif"],
+    },
   },
   plugins: [],
 };
