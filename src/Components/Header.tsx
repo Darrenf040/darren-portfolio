f906ba3351adfb5632f47f6bd8c7ce2d12a98ca6
Darrenf040 <darrenfr83@gmail.com>
2024-12-17 16:14:35 -0600

    transparent header background

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <header className="flex justify-between px-40 py-4 fixed w-full">
+      <header className="flex justify-between px-40 py-4 fixed w-full backdrop-blur-lg z-40">
         <a href="">DarrenF</a>
         <nav>
           <ul className="flex gap-12">
