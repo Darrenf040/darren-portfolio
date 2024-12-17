0054202f2ce99bbac3fc2e83bf9a674e00f768fa
Darrenf040 <darrenfr83@gmail.com>
2024-12-17 16:10:19 -0600

    hero section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import About from "./Sections/About";
+import Contact from "./Sections/Contact";
+import Hero from "./Sections/Hero";
+import Projects from "./Sections/Projects";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,24 @@
+import { Canvas, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { CanvasModel } from "../Components/Model";
+
+function Hero() {
+  return (
+    <section className="py-20 flex flex-col items-center">
+      <div
+        className=" flex flex-col relative h-screen w-full items-center"
+        id="canvas-container"
+      >
+        <div className="max-w-3xl text-accent-color">
+          <h1 className=" text-center">Hi, I'm Darren,</h1>
+          <h1 className=" text-center">Full Stack Developer</h1>
+        </div>
+        <CanvasModel />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
